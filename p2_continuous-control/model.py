9567aeb2b138f45fa1e806b91494f97d92011337
Yoonku Hwang <yz0624@gmail.com>
2018-12-27 15:46:44 +0900

    Trial #5 : Change critic learning rate

--- p2_continuous-control/model.py
+++ p2_continuous-control/model.py
@@ -12,8 +12,7 @@ def hidden_init(layer):
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed,\
-        fc1_units=512, fc2_units=512, fc3_units=512):
+    def __init__(self, state_size, action_size, seed, fc1_units=512, fc2_units=512, fc3_units=512):
         """Initialize parameters and build model.
         Params
         ======
@@ -48,8 +47,7 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed,
-        fcs1_units=512, fc2_units=512, fc3_units=512):
+    def __init__(self, state_size, action_size, seed, fcs1_units=512, fc2_units=512, fc3_units=512):
         """Initialize parameters and build model.
         Params
         ======
